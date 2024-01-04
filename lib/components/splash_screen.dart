6751c92d76f3d8bd0a094bae5bf80d3f311f08b2
mekdim12 <jrmekdim50@gmail.com>
2024-01-04 23:46:59 +0300

    splash screen added

---
+++ lib/components/splash_screen.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreenWidget extends StatefulWidget{
+  const SplashScreenWidget({super.key});
+
+  @override
+  State<SplashScreenWidget> createState() =>MainSplashScreenState();
+  
+}
+
+class MainSplashScreenState extends State<SplashScreenWidget> {
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            height: 250,
+            color: null,
+            decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/tg_main_logo.png'),
+                ),
+           )    
+          ),
+          const Text("Real State Consultancy", style: TextStyle(fontSize: 20, color: Colors.purple, fontWeight: FontWeight.bold),)
+        ],
+      ),
+    
+    );
+  }
+}
