a552fd9076ae6ca104f4bf28cb40ac8a0e722bf7
mekdim12 <jrmekdim50@gmail.com>
2024-01-21 12:01:22 +0300

    basic skeleton for page routing

---
+++ lib/components/pages/main_home_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+class MainHomePage extends StatelessWidget {
+  const MainHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Text("Main Home Page");
+  }
+}
