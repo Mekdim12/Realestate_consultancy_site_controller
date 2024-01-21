ad47a4666e5d2295fa604788721e46dc719f2c8c
mekdim12 <jrmekdim50@gmail.com>
2024-01-21 15:43:47 +0300

    ui for the main hompage is done using cards definied in small components

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,6 +1,11 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'small_components/card.dart';
+import 'pages/main_home_page.dart';
+import 'pages/brouchers_page.dart';
+import 'pages/contact_page.dart';
+import 'pages/profile_page.dart';
+import 'pages/properties_pages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,25 +15,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selected_index_page_sync_value_holder  = 2; // default value is 2 for home page
   @override
   Widget build(BuildContext context) {
+   
+    final screen_pages = <Widget>[
+        BrouchersPage(),
+        PropertiesPage(),
+        MainHomePage(),
+        ContactUserPage(),
+        ProfilePage()
+    ];
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CardWidget(500, 500) ,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[50],
+        elevation: 0,
+        title: Center(child: Column(
+          children:  [
+            Container(
+                width: 55,
+                height: 55,
+                // co?lor: null,
+                margin:const EdgeInsets.only(top: 10),
+                decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+
+                  image: AssetImage('assets/images/tg_main_logo.png'),
+                    ),
+              )    
+              ),
+            const Text("Home", style: TextStyle(fontSize: 12, color: Colors.white38) ,)
+          ],
+        
+        )),
+      ),
+      backgroundColor: Colors.grey[50],
+      body: IndexedStack(
+        index: selected_index_page_sync_value_holder,
+        children:screen_pages,
+      ),
       bottomNavigationBar: CurvedNavigationBar(
-      index: 2,
-       backgroundColor: Colors.white,
+      index: selected_index_page_sync_value_holder,
+       backgroundColor: Color.fromRGBO(250, 250, 250, 1.0),
        color:Colors.purple,
        animationDuration:const Duration(milliseconds: 200),
-       onTap: (value) {
-         
+       onTap: (index){
+        setState(() {
+           selected_index_page_sync_value_holder = index;
+        });
        },
-       items: const [
+       items: const <Widget> [
         Icon(Icons.post_add, color: Colors.white),
         Icon(Icons.add_card, color: Colors.white),
         Icon(Icons.home, color: Colors.white),
         Icon(Icons.contact_page, color: Colors.white),
-        Icon(Icons.settings, color: Colors.white)
+        Icon(Icons.account_circle_rounded, color: Colors.white)
        ],
 
       ),
--- lib/components/pages/main_home_page.dart
+++ lib/components/pages/main_home_page.dart
@@ -1,11 +1,62 @@
-
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-class MainHomePage extends StatelessWidget {
+import '../small_components/card.dart';
+
+class MainHomePage extends StatefulWidget {
   const MainHomePage({super.key});
 
   @override
+  State<MainHomePage> createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return  const Text("Main Home Page");
+    return Scaffold(
+        backgroundColor: Colors.grey[50],
+        body: Container(
+            child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  border: Border.all(
+                    color: Colors.deepPurple,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: const Center(
+                child: Text(
+                  "ETB 1,000,545,525 🤩",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CardWidget(150, 100, "User", "5984"),
+                    CardWidget(150, 100, "Call", "56"),
+                  ],
+                )),
+
+              Container(alignment: Alignment.center,
+                margin:const EdgeInsets.only(top:25),
+                child:CardWidget(
+                  300,
+                  100,
+                  "New Call Request",
+                  "6",
+                ))
+          ],
+        )));
   }
 }
