054bfb3e30e69579a67ea114f5c9dbdfb296d455
mekdim12 <jrmekdim50@gmail.com>
2024-01-25 23:53:38 +0300

    adding new detail page and plan doc string

---
+++ lib/components/pages/brouchers_detail_page.dart
@@ -0,0 +1,3 @@
+//  pass context either you use this either to update and delete the brouchers info other wise to upload some
+//  like 1 with id and everything to be updated and deleted 
+// 2 for adding new item with floating button
