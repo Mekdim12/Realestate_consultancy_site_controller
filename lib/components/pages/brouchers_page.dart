429f13a2f5027fce25b3e55232a6931e62234022
mekdim12 <jrmekdim50@gmail.com>
2024-01-25 23:34:58 +0300

    included some sample data to make list viewer of brouchers and list viewer it self draft

--- lib/components/pages/brouchers_page.dart
+++ lib/components/pages/brouchers_page.dart
@@ -10,24 +10,33 @@ class BrouchersPage extends StatefulWidget {
 }
 
 class BrouchersData{
-  String nameOfTheFile="";
-  String typeOfTheFile="";
-  int numberLabel = 1;
+  final String nameOfTheFile; // tittle
+  final String filePath; // image
+  final String descritpion; // description
+  final String  type; // type
+  
+  const BrouchersData({
+    required this.nameOfTheFile,
+    required this.descritpion,
+    required this.filePath,
+    required this.type
+  });
 }
 
 class _BrouchersPageState extends State<BrouchersPage> {
   // fall back as place holder for image slider in brouchers page
-  final list_of_brouchers_of_offers = [
-    "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/3646108/pexels-photo-3646108.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/3879065/pexels-photo-3879065.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/1841120/pexels-photo-1841120.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/3608542/pexels-photo-3608542.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/3786092/pexels-photo-3786092.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/3166786/pexels-photo-3166786.jpeg?auto=compress&cs=tinysrgb&w=600"
-  ];
+  // final list_of_brouchers_of_offers = [
+  //   "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/3646108/pexels-photo-3646108.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/3879065/pexels-photo-3879065.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/1841120/pexels-photo-1841120.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/3608542/pexels-photo-3608542.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/3786092/pexels-photo-3786092.jpeg?auto=compress&cs=tinysrgb&w=600",
+  //   "https://images.pexels.com/photos/3166786/pexels-photo-3166786.jpeg?auto=compress&cs=tinysrgb&w=600"
+  // ];
+
   //  local dev place for slider image when working with no internet
-  final list_of_brouchers_of_offers_local = [
+  final list_of_brouchers_of_offers = [
     AssetImage('assets/images/not_found.png'),
     AssetImage('assets/images/not_found.png'),
     AssetImage('assets/images/not_found.png'),
@@ -37,9 +46,56 @@ class _BrouchersPageState extends State<BrouchersPage> {
     AssetImage('assets/images/not_found.png')
   ];
 
+  final listOfBroucherItems = [
+   const BrouchersData(
+      nameOfTheFile:"Mekdim Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type:"main"
+    ),  
+    const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+    const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+     const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+     const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+     const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+     const BrouchersData(
+     nameOfTheFile:"Etsub Tamirat",
+      filePath:"assets/images/not_found.png",
+      descritpion: "This lorem Ipsum Identification",
+      type :"other"
+    ),
+   
+  ];
+
   @override
   Widget build(BuildContext context) {
     bool is_image_available = list_of_brouchers_of_offers.length > 0;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -49,7 +105,9 @@ class _BrouchersPageState extends State<BrouchersPage> {
         child: const Icon(Icons.add),
       ),
       body: is_image_available
-          ? Container(
+          ? Column(
+            children: [
+             Container(
               margin: const EdgeInsets.symmetric(horizontal:10, vertical: 20 ),
               child: CarouselSlider.builder(
                 options: CarouselOptions(
@@ -68,22 +126,37 @@ class _BrouchersPageState extends State<BrouchersPage> {
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                          image: DecorationImage(
-                            image:list_of_brouchers_of_offers_local[itemIndex],
+                            image:list_of_brouchers_of_offers[itemIndex],
                             fit: BoxFit.fitWidth),
                             ),
                   );
                 },
-              ))
+              )) ,
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                height: screenHeight - 400,
+                child: ListView.builder(
+                  itemCount:listOfBroucherItems.length,
+                  itemBuilder: (context, index){
+                    print("xxxxxxxxxxxxxx");
+                    return ListTile(
+                      title: Text("Mekdim TAMIRAT"),
+                      subtitle : Text("sibSDFSDF"),
+                      trailing: Icon(Icons.arrow_forward),
+                    );
+                  }
+                  ) ,
+              )
+   ] )
           : SizedBox(
               width: double.infinity,
-              height: 200,
+              height: double.infinity,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
                       width: 105,
                       height: 105,
-                      margin: const EdgeInsets.only(top: 10),
                       decoration: const BoxDecoration(
                         image: DecorationImage(
                           fit: BoxFit.cover,
@@ -92,7 +165,7 @@ class _BrouchersPageState extends State<BrouchersPage> {
                         ),
                       )),
                   const Text(
-                    "No Brouchers Found",
+                    "No Brouchers Found +",
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   )
                 ],
