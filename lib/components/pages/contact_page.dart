2c46cf3f5f1b56abf033faefc9ed7b2c5a8b2287
mekdim12 <jrmekdim50@gmail.com>
2024-01-30 23:48:29 +0300

    include skelton of the tabs for contacting page

--- lib/components/pages/contact_page.dart
+++ lib/components/pages/contact_page.dart
@@ -11,6 +11,45 @@ class ContactUserPage extends StatefulWidget {
 class _ContactUserPageState extends State<ContactUserPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Contacting page");
+    return Container(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      width: double.infinity,
+      height: double.infinity,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.white70,
+            bottom: const TabBar(
+              // indicatorColor: Colors.greenAccent,
+              dividerColor: Colors.deepPurple,
+              // labelColor: Colors.greenAccent,
+              // indicatorSize: TabBarIndicatorSize.tab,
+              labelColor: Colors.purple,
+              tabs: [
+                Tab(
+                    text: "Text Based",
+                    icon: Icon(
+                      Icons.contact_mail,
+                      color: Colors.purple,
+                    )),
+                Tab(
+                  iconMargin: EdgeInsets.all(5),
+                  text: "Image Based",
+
+                  icon: Icon(
+                    Icons.call,
+                    color: Colors.purple,
+                  ),
+                )
+              ],
+            ),
+          ),
+          body: TabBarView(children: [Text("data"), Text("2")]),
+        ),
+      ),
+    );
   }
 }
