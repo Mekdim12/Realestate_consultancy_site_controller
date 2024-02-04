9819575f1042878d8bfe2391551fa355b158454d
mekdim12 <jrmekdim50@gmail.com>
2024-02-04 13:06:35 +0300

    profile status detail insert and updating page done

--- lib/components/pages/profile_page.dart
+++ lib/components/pages/profile_page.dart
@@ -1,11 +1,137 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Text("Profile Page");
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      height: MediaQuery.of(context).size.height - 100,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 25, bottom: 15),
+              child: const Text(
+                "STATUS",
+                style: TextStyle(
+                    color: Color.fromARGB(159, 72, 59, 6),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, top: 5),
+              child: Row(
+                children: [
+                  const Text(
+                    "\$",
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF483b06)),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 15),
+                    width: screenWidth * 0.8,
+                    child: const TextField(
+                      // controller: _passwordOfAdmin,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                      ),
+                      cursorColor: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, top: 25),
+              child: Row(
+                children: [
+                  const Text(
+                    "\$",
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF483b06)),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 15),
+                    width: screenWidth * 0.8,
+                    child: const TextField(
+                      // controller: _passwordOfAdmin,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                      ),
+                      cursorColor: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, top: 25),
+              child: Row(
+                children: [
+                  const Text(
+                    "\$",
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF483b06)),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 15),
+                    width: screenWidth * 0.8,
+                    child: const TextField(
+                      // controller: _passwordOfAdmin,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                      ),
+                      cursorColor: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 150,
+              margin: const EdgeInsets.only(top: 25),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  alignment: Alignment.bottomCenter,
+                  backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  elevation: MaterialStatePropertyAll(15),
+                ),
+                onPressed: () {},
+                child: const Text("Update")))
+          ],
+        ),
+      ),
+    );
   }
 }
