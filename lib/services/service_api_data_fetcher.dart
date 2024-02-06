700a29e4d3950cbd2d2f69c884ea244573b26a57
mekdim12 <jrmekdim50@gmail.com>
2024-02-06 23:26:13 +0300

    returning the actual response from the server

--- lib/services/service_api_data_fetcher.dart
+++ lib/services/service_api_data_fetcher.dart
@@ -3,16 +3,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-// use local host 8000 for testing and fetch data using the json format
-
-
 class ApiFetcherService{
   Future fetchLandingPageStatusData() async {
     Uri url = Uri.parse('http://127.0.0.1:8000/api');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(json.decode(response.body));
-      return [];
+      return json.decode(response.body);
     }
     return [];
   }
