7c61f1d263bb9a6c9d257340ad9ca09ea8842b18
= <jrmekdim50@gmail.com>
2024-02-10 17:11:16 +0300

    add api service handler for status profile page for getting and uploading the status of company

--- lib/services/service_api_data_fetcher.dart
+++ lib/services/service_api_data_fetcher.dart
@@ -5,12 +5,34 @@ import 'package:http/http.dart' as http;
 
 class ApiFetcherService{
   Future fetchLandingPageStatusData() async {
-    Uri url = Uri.parse('http://127.0.0.1:8000/api');
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/home');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       return json.decode(response.body);
     }
     return [];
   }
+
+  Future fetchProfileStatusData() async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/profile');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    }
+    return [];
   }
+  Future updateProfileStatusData(String totalSales, String averageSoldPrice, String companies) async {
+       Uri url = Uri.parse('http://127.0.0.1:8000/api/profile');
+        final response = await http.post(url, body: {
+          'total_sales': totalSales,
+          'average_sold_price': averageSoldPrice,
+          'companies': companies
+        });
+        if (response.statusCode == 200) {
+          return json.decode(response.body);
+        }
+        return [];
+
+  }
+}
 
