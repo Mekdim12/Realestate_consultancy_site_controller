6916e183274c332766a1b8882fccf59cba370734
= <jrmekdim50@gmail.com>
2024-02-20 23:35:37 +0300

    define contact me message from clients model

---
+++ lib/models/contactsMe.dart
@@ -0,0 +1,14 @@
+class ContactMeMessage {
+  final String clientFullName;
+  final String clientEmail;
+  final String clientSubjectOfMessage;
+  final String clienFullMessage;
+  final String id;
+
+  const ContactMeMessage(
+      {required this.clientFullName,
+      required this.clientEmail,
+      required this.clientSubjectOfMessage,
+      required this.clienFullMessage,
+      required this.id});
+}
