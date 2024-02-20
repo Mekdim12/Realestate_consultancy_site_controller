abeb59fcc34f4de678469668b62b4ae78cdfac05
= <jrmekdim50@gmail.com>
2024-02-20 23:35:16 +0300

    define client requested call model

---
+++ lib/models/clientCallRequest.dart
@@ -0,0 +1,16 @@
+class CallRequestedClient {
+  final bool status;
+  final String typeOfAnItem;
+  final String id;
+  final String clientName;
+  final String clienEmail;
+  final String phoneNumber;
+
+  const CallRequestedClient(
+      {required this.status,
+      required this.typeOfAnItem,
+      required this.id,
+      required this.clientName,
+      required this.clienEmail,
+      required this.phoneNumber});
+}
