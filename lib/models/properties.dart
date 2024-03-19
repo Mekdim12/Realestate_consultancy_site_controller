69b7e48428ef78e7d01cf9d568de8f5f4b6a5a9d
= <jrmekdim50@gmail.com>
2024-03-19 23:30:26 +0300

    preparing model for peroperties datas

--- lib/models/properties.dart
+++ lib/models/properties.dart
@@ -1,7 +1,37 @@
-class PropertiesData{
+class PropertyVehiclesData{
 
-final String name;
-  const PropertiesData({
-    required this.name
+final String city;
+final String subCity;
+final String description;
+final String unitsAvailabel;
+final String neighborhoodOrStreetName;
+final String buildingFloors;
+final String pricePerSq;
+final String numberOfBedrooms;
+final String lastUpdate;
+final String status;
+final String realstateCompanyName;
+
+
+  const PropertyVehiclesData({
+    required this.city,
+    required this.subCity,
+    required this.description,
+    required this.unitsAvailabel,
+    required this.neighborhoodOrStreetName,
+    required this.buildingFloors,
+    required this.pricePerSq,
+    required this.numberOfBedrooms,
+
+
   });
 }
+
+
+class PropertyRealstateData{
+
+
+}
+
+status = models.CharField(max_length=100)
+real_state_company_name = models.CharField(max_length=100)
