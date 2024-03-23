44dc6a52dd2b44413c2823a7c47701e85b3ed698
= <jrmekdim50@gmail.com>
2024-03-23 16:34:11 +0300

    Add id field to PropertyRealstateData and PropertyVehiclesData

--- lib/models/properties.dart
+++ lib/models/properties.dart
@@ -11,6 +11,7 @@ class PropertyRealstateData {
   final String status;
   final String realstateCompanyName;
   final String propertyImage;
+  final String id;
 
   const PropertyRealstateData({
     required this.city,
@@ -25,7 +26,32 @@ class PropertyRealstateData {
     required this.status,
     required this.realstateCompanyName,
     required this.propertyImage,
+    required this.id,
   });
+
+  factory PropertyRealstateData.fromJson(Map<String, dynamic> json) {
+    return PropertyRealstateData(
+      propertyImage: json['propery_image'],
+      city: json['city'],
+      subCity: json['sub_city'],
+      description: json['description'],
+      unitsAvailabel: json['units_available'].toString(),
+      neighborhoodOrStreetName: json['neighborhood_or_street_name'],
+      buildingFloors: json['building_floors'].toString(),
+      pricePerSq: json['price_per_sq'].toString(),
+      numberOfBedrooms: json['number_of_bedrooms'].toString(),
+      lastUpdate: json['last_update'],
+      status: json['status'],
+      realstateCompanyName: json['real_state_company_name'],
+      id: json['id'].toString(),
+    );
+  }
+
+  static List<PropertyRealstateData> fromJsonList(List<dynamic> jsonList) {
+    return jsonList
+        .map((json) => PropertyRealstateData.fromJson(json))
+        .toList();
+  }
 }
 
 class PropertyVehiclesData {
@@ -39,6 +65,7 @@ class PropertyVehiclesData {
   final String userOrNew;
   final String status;
   final String vehicleImage;
+  final String id;
 
   const PropertyVehiclesData({
     required this.nameOfTheCar,
@@ -51,5 +78,26 @@ class PropertyVehiclesData {
     required this.userOrNew,
     required this.status,
     required this.vehicleImage,
+    required this.id,
   });
+
+  factory PropertyVehiclesData.fromJson(Map<String, dynamic> json) {
+    return PropertyVehiclesData(
+      vehicleImage: json['vehicle_image'],
+      nameOfTheCar: json['name_of_the_car'],
+      color: json['color'],
+      plateNumberCity: json['plate_number_city'],
+      description: json['description'],
+      unitsAvailabel: json['units_available'].toString(),
+      price: json['price'].toString(),
+      lastUpdate: json['last_update'],
+      userOrNew: json['used_or_new'],
+      status: json['status'],
+      id: json['id'].toString(),
+    );
+  }
+
+  static List<PropertyVehiclesData> fromJsonList(List<dynamic> jsonList) {
+    return jsonList.map((json) => PropertyVehiclesData.fromJson(json)).toList();
+  }
 }
