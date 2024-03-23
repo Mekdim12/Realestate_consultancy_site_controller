ce2dca6e34eb9a0e8e8bbf47135a371bb5dddf4f
= <jrmekdim50@gmail.com>
2024-03-23 23:09:52 +0300

    define api caller service for properties items

--- lib/services/service_api_data_fetcher.dart
+++ lib/services/service_api_data_fetcher.dart
@@ -1,11 +1,11 @@
-
 // import json and http package
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/brouchersAndSpecialOffer.dart';
 import 'package:flutter/material.dart';
+import '../models/properties.dart';
 
-class ApiFetcherService{
+class ApiFetcherService {
   Future fetchLandingPageStatusData() async {
     Uri url = Uri.parse('http://127.0.0.1:8000/api/home');
     final response = await http.get(url);
@@ -23,66 +23,103 @@ class ApiFetcherService{
     }
     return [];
   }
-  Future updateProfileStatusData(String totalSales, String averageSoldPrice, String companies) async {
-       Uri url = Uri.parse('http://127.0.0.1:8000/api/profile');
-        final response = await http.post(url, body: {
-          'total_sales': totalSales,
-          'average_sold_price': averageSoldPrice,
-          'companies': companies
-        });
-        if (response.statusCode == 200) {
-          return json.decode(response.body);
-        }
-        return [];
 
+  Future updateProfileStatusData(
+      String totalSales, String averageSoldPrice, String companies) async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/profile');
+    final response = await http.post(url, body: {
+      'total_sales': totalSales,
+      'average_sold_price': averageSoldPrice,
+      'companies': companies
+    });
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    }
+    return [];
   }
 
+  Future addBrouchersAndBestOfferes(String nameOfTheFile, String description,
+      String type, String filePath) async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
+    // use multipart to send file and text data
 
+    var request = http.MultipartRequest('POST', url)
+      ..fields['title'] = nameOfTheFile
+      ..fields['description'] = description
+      ..fields['type'] = type
+      ..files.add(await http.MultipartFile.fromPath('image', filePath));
+    var response = await request.send();
+    if (response.statusCode == 201) {
+      return true;
+    }
+    return false;
+  }
 
-    Future addBrouchersAndBestOfferes(String nameOfTheFile, String description, String type, String filePath) async {
-              Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
-              // use multipart to send file and text data
-              
-              var request = http.MultipartRequest('POST', url)
-                ..fields['title'] = nameOfTheFile
-                ..fields['description'] = description
-                ..fields['type'] = type
-                ..files.add(await http.MultipartFile.fromPath('image', filePath));
-              var response = await request.send();
-              if (response.statusCode == 201) {
-                return true;
-              }
-              return false;
-    } 
-    Future fetchBrouchersAndBestOfferes() async {
-       Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
-       final response = await http.get(url);
-       
-        if(response.body.isEmpty){
-          return [];
-        }
+  Future fetchBrouchersAndBestOfferes() async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
+    final response = await http.get(url);
 
-        final parsed = jsonDecode(response.body);
-      
-        return BrouchersData.fromJsonList(parsed);
-        
-      }
+    if (response.body.isEmpty) {
+      return [];
+    }
 
-      Future deleteBrocuherAndBestofferitem(String id) async{
-         Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
-          await http.delete(url, body: {
-            'id': id
-          });
-      }
+    final parsed = jsonDecode(response.body);
 
+    return BrouchersData.fromJsonList(parsed);
+  }
 
-      Future fetchContactMe() async{
-        Uri url = Uri.parse('http://127.0.0.1:8000/api/contact');
-        final response = await http.get(url);
+  Future deleteBrocuherAndBestofferitem(String id) async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/broucher');
+    await http.delete(url, body: {'id': id});
+  }
+
+  Future fetchContactMe() async {
+    Uri url = Uri.parse('http://127.0.0.1:8000/api/contact');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    }
+    return [];
+  }
+
+  Future fetchProperties() async {
+    try {
+      Uri urlRealEstate =
+          Uri.parse('http://127.0.0.1:8000/api/property/realestate');
+      Uri urlVehicles =
+          Uri.parse('http://127.0.0.1:8000/api/property/vehicle');
+
+      // Perform requests concurrently
+      final responses = await Future.wait([
+        http.get(urlRealEstate),
+        http.get(urlVehicles),
+      ]);
+
+      final finalResponse = [];
+
+      for (var response in responses) {
         if (response.statusCode == 200) {
-          return json.decode(response.body);
+          try {
+            final responseBody = jsonDecode(response.body);
+            if (response.request?.url == urlRealEstate) {
+              finalResponse
+                  .add(PropertyRealstateData.fromJsonList(responseBody));
+            } else if (response.request?.url == urlVehicles) {
+              finalResponse
+                  .add(PropertyVehiclesData.fromJsonList(responseBody));
+            }
+          } catch (e) {
+            print('Error decoding JSON: $e');
+            print('Response body: ${response.body}');
+          }
+        } else {
+          print('Request failed with status: ${response.statusCode}');
+          print('Response body: ${response.body}');
         }
-        return [];
       }
-
+      return finalResponse;
+    } catch (e) {
+      return [];
+    }
+  }
 }
