007c4a3372ce49fbab3e3e444ae11e9cbb8cb236
= <jrmekdim50@gmail.com>
2024-03-25 22:27:46 +0300

    updating main list view builder

--- lib/components/pages/properties_main_page.dart
+++ lib/components/pages/properties_main_page.dart
@@ -4,7 +4,7 @@ import '../../services/service_api_data_fetcher.dart';
 import '../../models/brouchersAndSpecialOffer.dart';
 
 import '../../models/properties.dart';
-
+import './properties_detail_page.dart';
 
 class PropertiesMainPage extends StatefulWidget {
   const PropertiesMainPage({super.key});
@@ -16,13 +16,13 @@ class PropertiesMainPage extends StatefulWidget {
 class _PropertiesMainPageState extends State<PropertiesMainPage> {
   ApiFetcherService apiService = ApiFetcherService();
   List listOfPropertiesItems = [];
-  
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     return FutureBuilder(
-        future:apiService.fetchProperties(),
+        future: apiService.fetchProperties(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
@@ -31,74 +31,127 @@ class _PropertiesMainPageState extends State<PropertiesMainPage> {
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}'); // Show error ifc any
           } else {
-            print( snapshot.data);
-            print("-------------------------------------------->");
-            // listOfBroucherItems = snapshot.data;
             listOfPropertiesItems = snapshot.data;
-            print(listOfPropertiesItems);
-            // return Scaffold(
-            //   body: Center(child: Text('No Properties Found')),
-            // );
-            bool is_image_available = listOfPropertiesItems.length > 0;
+
+            bool is_available = listOfPropertiesItems.length > 0;
             return Scaffold(
               floatingActionButton: FloatingActionButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            PropertiesPage()),
+                    MaterialPageRoute(builder: (context) => PropertiesPage()),
                   );
                 },
                 backgroundColor: Colors.purple,
                 child: const Icon(Icons.add),
               ),
-              body: is_image_available
+              body: is_available
                   ? Column(children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 5, bottom: 20),
+                        child: Center(
+                            child: Text(
+                          "🏡/🚗",
+                          style: TextStyle(fontSize: 30),
+                        )),
+                      ),
                       Container(
                         margin: const EdgeInsets.only(
                             bottom: 10, left: 10, right: 10),
                         height: screenHeight - 400,
                         child: ListView.builder(
-                            itemCount: listOfBroucherItems.length,
-                            itemBuilder: (context, index) {
-                              var currentItem = listOfBroucherItems[index];
-                              return ListTile(
-                                onTap: () => {},
-                                iconColor: Colors.purple,
-                                selectedColor: Colors.deepPurple,
-                                enableFeedback: true,
-                                hoverColor: Color.fromARGB(89, 104, 58, 183),
-                                contentPadding: const EdgeInsets.all(5),
-                                leading: Image(
-                                    image: NetworkImage(currentItem.filePath)),
-                                title: Text(
-                                  currentItem.nameOfTheFile,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.purple),
+                          itemCount: listOfPropertiesItems.length,
+                          itemBuilder: (context, index) {
+                            var currentItem = listOfPropertiesItems[index];
+                            return CustomScrollView(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              slivers: <Widget>[
+                                SliverList(
+                                  delegate: SliverChildBuilderDelegate(
+                                    (BuildContext context, int subIndex) {
+                                      var subItem = currentItem[subIndex];
+                                      Widget currentTile;
+                                      if (subItem is PropertyRealstateData) {
+                                        // Handle PropertyRealstateData
+                                        currentTile = Container(
+                                          margin: EdgeInsets.only(bottom: 10),
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Color.fromARGB(37, 0, 0,
+                                                    0)), // Change this to your preferred border color
+                                            borderRadius: BorderRadius.circular(
+                                                10), // Change this to your preferred border radius
+                                          ),
+                                          child: ListTile(
+                                            iconColor: Colors.purple,
+                                            selectedColor: Colors.deepPurple,
+                                            enableFeedback: true,
+                                            hoverColor: Color.fromARGB(
+                                                89, 104, 58, 183),
+                                            contentPadding:
+                                                const EdgeInsets.all(5),
+                                            leading: Icon(Icons.home),
+                                            title: Text(
+                                                "${subItem.realstateCompanyName}"),
+                                            subtitle: Text(
+                                                "\$ ${subItem.pricePerSq}"),
+                                          ),
+                                        );
+                                      } else if (subItem
+                                          is PropertyVehiclesData) {
+                                        // Handle PropertyVehiclesData
+                                        currentTile = Container(
+                                          margin: EdgeInsets.only(bottom: 10),
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Color.fromARGB(37, 0, 0,
+                                                    0)), // Change this to your preferred border color
+                                            borderRadius: BorderRadius.circular(
+                                                10), // Change this to your preferred border radius
+                                          ),
+                                          child: ListTile(
+                                            iconColor: Colors.purple,
+                                            selectedColor: Colors.deepPurple,
+                                            enableFeedback: true,
+                                            hoverColor: Color.fromARGB(
+                                                89, 104, 58, 183),
+                                            contentPadding:
+                                                const EdgeInsets.all(5),
+                                            leading:
+                                                Icon(Icons.car_rental_rounded),
+                                            title:
+                                                Text("${subItem.nameOfTheCar}"),
+                                            subtitle:
+                                                Text("\$ ${subItem.price}"),
+                                          ),
+                                        );
+                                      } else {
+                                        // Handle other types
+                                        currentTile = ListTile();
+                                      }
+
+                                      return InkWell(
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    PropertiesDetailPage(1, "")),
+                                          );
+                                        },
+                                        child: currentTile,
+                                        splashColor: Colors
+                                            .purpleAccent, // Change this to your preferred splash color
+                                      );
+                                    },
+                                    childCount: currentItem.length,
+                                  ),
                                 ),
-                                subtitle: (currentItem.description
-                                            .toString()
-                                            .length >
-                                        50)
-                                    ? Text(
-                                        "${currentItem.description.toString().substring(0, 50)}...")
-                                    : Text(
-                                        "${currentItem.description.toString().substring(0, currentItem.description.toString().length)}..."),
-                                trailing: ElevatedButton.icon(
-                                  onPressed: () {
-                                    apiService.deleteBrocuherAndBestofferitem(
-                                        currentItem.id.toString());
-                                    setState(() {
-                                      listOfBroucherItems.removeAt(index);
-                                    });
-                                  },
-                                  icon: const Icon(Icons.delete),
-                                  label: const Text(''),
-                                ),
-                              );
-                            }),
+                              ],
+                            );
+                          },
+                        ),
                       )
                     ])
                   : SizedBox(
