722bd993e0c1a430b39b71152cf3d5f4dbcbb426
= <jrmekdim50@gmail.com>
2024-03-28 23:12:10 +0300

    removing uncessary import

--- lib/components/pages/brouchers_page.dart
+++ lib/components/pages/brouchers_page.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import '../../services/service_api_data_fetcher.dart';
 import './brouchers_detail_page.dart';
