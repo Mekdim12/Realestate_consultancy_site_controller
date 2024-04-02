5342abe00bdfc9bfcb5542c52d4e9db693df642a
= <jrmekdim50@gmail.com>
2024-04-02 22:45:59 +0300

    added service for adding realestate to database

--- lib/services/service_api_data_add_and_updater.dart
+++ lib/services/service_api_data_add_and_updater.dart
@@ -45,4 +45,48 @@ class ApiDataUploaderAndUpdater {
       return false;
     }
   }
+
+  Future addRealestatePropertyData(
+      String city,
+      String subcity,
+      String descritpion,
+      String unitsAavilabel,
+      String neighborhoodOrStreetName,
+      String buildingFloors,
+      String pricePerSqfeet,
+      String numberOfBedRooms,
+      String status,
+      String realestateName,
+      List listOfImages) async {
+    Dio dio = Dio();
+
+    Uri url = Uri.parse('http://localhost:8000/api/property/realestate');
+
+    List<MultipartFile> multipartFiles =
+        await Future.wait(listOfImages.map((image) async {
+      return MultipartFile.fromFile(image.path,
+          filename: image.path.split('/').last);
+    }));
+
+    FormData formData = FormData.fromMap({
+      'city': city,
+      'sub_city': subcity,
+      'description': descritpion,
+      'units_available': unitsAavilabel,
+      'neighborhood_or_street_name': neighborhoodOrStreetName,
+      'building_floors': buildingFloors,
+      'price_per_sq': pricePerSqfeet,
+      'number_of_bedrooms': numberOfBedRooms,
+      'status': status,
+      'real_state_company_name': realestateName,
+      'realestate_image': multipartFiles
+    });
+    final response = await dio.post(url.toString(), data: formData);
+
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      return false;
+    }
+  }
 }
