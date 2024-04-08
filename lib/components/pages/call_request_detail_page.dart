994093b9f556c226524748c73b8578d4d5ebfa18
= <jrmekdim50@gmail.com>
2024-04-08 23:51:03 +0300

    updating contacing page so it can use the api

--- lib/components/pages/call_request_detail_page.dart
+++ lib/components/pages/call_request_detail_page.dart
@@ -1,83 +1,201 @@
-
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import '../../services/service_api_data_add_and_updater.dart';
+import '../../models/clientCallRequest.dart';
+
+typedef OnUpdateCallback = Function(dynamic updatedObject);
 
 class CallRequestDetailPage extends StatefulWidget {
   final requstedItem;
-  const CallRequestDetailPage(this.requstedItem , {super.key});
-
+  final OnUpdateCallback onUpdate;
+  const CallRequestDetailPage(this.requstedItem,
+      {Key? key, required this.onUpdate})
+      : super(key: key);
   @override
   State<CallRequestDetailPage> createState() => _CallRequestDetailPageState();
 }
 
 class _CallRequestDetailPageState extends State<CallRequestDetailPage> {
+  CallRequestedClient? contactObject;
+
+  ApiDataUploaderAndUpdater deleteContactInformation =
+      ApiDataUploaderAndUpdater();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      contactObject = widget.requstedItem;
+    });
+  }
+
+  void saveChanges() {
+    if (widget.onUpdate != null) {
+      widget.onUpdate!(contactObject);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final requestedItmObject = widget.requstedItem;
-    final  screenHeight = MediaQuery.of(context).size.height;
+    final screenHeight = MediaQuery.of(context).size.height;
     final screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Call Requests Detail"),
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Call Requests Detail"),
+          ),
         ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: screenHeight,
-        margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-        child: Column(
-          children: [
-
+        body: Container(
+          width: double.infinity,
+          height: screenHeight,
+          margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+          child: Column(
+            children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Full Name :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(requestedItmObject.clientName, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Full Name :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      requestedItmObject.clientName,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Client Email :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(requestedItmObject.clienEmail, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Client Email :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      requestedItmObject.clienEmail,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-             
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Proprty Type :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(requestedItmObject.typeOfAnItem, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Proprty Type :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      requestedItmObject.typeOfAnItem,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Phone Number :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text(requestedItmObject.phoneNumber, style: TextStyle(fontWeight: FontWeight.w500),), Container(margin: EdgeInsets.symmetric(horizontal: 10),), ElevatedButton.icon(onPressed: (){}, style: ButtonStyle(backgroundColor:MaterialStatePropertyAll(Colors.grey[50])),icon: Icon(Icons.copy, color: Colors.deepPurple,), label: Text(""))],
-                  ),)
+                  Container(
+                    child: Text(
+                      "Phone Number :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          requestedItmObject.phoneNumber,
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                        ),
+                        ElevatedButton.icon(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(Colors.grey[50])),
+                            icon: Icon(
+                              Icons.copy,
+                              color: Colors.deepPurple,
+                            ),
+                            label: Text(""))
+                      ],
+                    ),
+                  )
                 ],
               ),
-              
-              Container(margin: EdgeInsets.symmetric(vertical: 50),),
-
-             ElevatedButton.icon(onPressed: ()=>{}, icon: Icon(Icons.delete, color: Colors.white,), label:Text("Delete"), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),)
-          ],
-        ),
-      )
-    );
- 
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 50),
+              ),
+              ElevatedButton.icon(
+                onPressed: () async {
+                  print(requestedItmObject.id);
+                  bool response = await deleteContactInformation
+                      .deleteContactInformation(requestedItmObject.id, '2');
+                  if (response == true) {
+                    saveChanges();
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(
+                          const SnackBar(
+                              content: Text("Contact Information Deleted")),
+                        )
+                        .closed
+                        .then((_) {
+                      Navigator.pop(context);
+                    });
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Contact Information not Deleted")));
+                  }
+                },
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+                label: Text("Delete"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/components/pages/contact_me_detail_page.dart
+++ lib/components/pages/contact_me_detail_page.dart
@@ -1,74 +1,183 @@
 import 'package:flutter/material.dart';
 import 'package:realstateconsultancy/components/pages/contact_page.dart';
+import '../../services/service_api_data_add_and_updater.dart';
+import '../../models/contactsMe.dart';
+
+typedef OnUpdateCallback = Function(dynamic updatedObject);
 
 class ContactMeDetailPage extends StatefulWidget {
   final contact_message;
-  const ContactMeDetailPage( this.contact_message ,{super.key});
+  final OnUpdateCallback onUpdate;
+  const ContactMeDetailPage(this.contact_message,
+      {Key? key, required this.onUpdate})
+      : super(key: key);
 
   @override
   State<ContactMeDetailPage> createState() => _ContactMeDetailPageState();
 }
 
 class _ContactMeDetailPageState extends State<ContactMeDetailPage> {
+  ContactMeMessage? contactObject;
+  ApiDataUploaderAndUpdater deleteContactInformation =
+      ApiDataUploaderAndUpdater();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      contactObject = widget.contact_message;
+    });
+  }
+
+  void saveChanges() {
+    if (widget.onUpdate != null) {
+      widget.onUpdate!(contactObject);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final contact_message = widget.contact_message;
-    final  screenHeight = MediaQuery.of(context).size.height;
+    final screenHeight = MediaQuery.of(context).size.height;
     final screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Contact Me Detail Page"),
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Contact Me Detail Page"),
+          ),
         ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: screenHeight,
-        margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-        child: Column(
-          children: [
-
+        body: Container(
+          width: double.infinity,
+          height: screenHeight,
+          margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+          child: Column(
+            children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Full Name :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(contact_message.clientFullName, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Full Name :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      contact_message.clientFullName,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Client Email :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(contact_message.clientEmail, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Client Email :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      contact_message.clientEmail,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-             
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Message Subject :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                  Container(margin:const EdgeInsets.symmetric(horizontal: 10),),
-                  Container( child: Text(contact_message.clientSubjectOfMessage, style: TextStyle(fontWeight: FontWeight.w500),),)
+                  Container(
+                    child: Text(
+                      "Message Subject :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                  Container(
+                    child: Text(
+                      contact_message.clientSubjectOfMessage,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
                 ],
               ),
-              Container(margin: EdgeInsets.symmetric(vertical: 5),),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container( child:Text("Full Message :", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple),),),
-                 Container( width: screenwidth * 0.55, child: Text(contact_message.clienFullMessage, style: TextStyle(fontWeight: FontWeight.w500), softWrap: true, maxLines: 150, overflow: TextOverflow.fade,))
+                  Container(
+                    child: Text(
+                      "Full Message :",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.purple),
+                    ),
+                  ),
+                  Container(
+                      width: screenwidth * 0.55,
+                      child: Text(
+                        contact_message.clienFullMessage,
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                        softWrap: true,
+                        maxLines: 150,
+                        overflow: TextOverflow.fade,
+                      ))
                 ],
               ),
-              Container(margin: EdgeInsets.symmetric(vertical: 50),),
-
-             ElevatedButton.icon(onPressed: ()=>{}, icon: Icon(Icons.delete, color: Colors.white,), label:Text("Delete"), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),)
-          ],
-        ),
-      )
-    );
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 50),
+              ),
+              ElevatedButton.icon(
+                onPressed: () async {
+                  bool response = await deleteContactInformation
+                      .deleteContactInformation(contact_message.id, '1');
+                  if (response == true) {
+                    saveChanges();
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(
+                          const SnackBar(
+                              content: Text("Contact Information Deleted")),
+                        )
+                        .closed
+                        .then((_) {
+                      Navigator.pop(context);
+                    });
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Contact Information not Deleted")));
+                  }
+                },
+                icon:const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+                label: Text("Delete"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+              )
+            ],
+          ),
+        ));
   }
 }
