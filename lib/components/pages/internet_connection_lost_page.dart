596aa4bc0e5178adecaf605a179fbab52b3a8353
mekdim12 <jrmekdim50@gmail.com>
2024-04-17 10:43:20 +0300

    Refactor internet_connection_lost_page.dart to display a no internet connection page

--- lib/components/pages/internet_connection_lost_page.dart
+++ lib/components/pages/internet_connection_lost_page.dart
@@ -4,8 +4,46 @@ class NoInternetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('No internet connection'),
+      body: Container(
+        margin: const EdgeInsets.only(top: 20),
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              color: null,
+              margin: const EdgeInsets.only(top: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/tg_main_logo.png'),
+                ),
+              ),
+            ),
+            //  load the no_internet gif here from the assets gif
+
+            Image.asset(
+              'assets/gif/no_internet.gif',
+              width: 500,
+              height: 500,
+            ),
+            const SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    '📶📶❌ No Internet Connection 📶📶❌',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
+          ],
+        ),
       ),
     );
   }
