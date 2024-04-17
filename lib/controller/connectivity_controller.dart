b76c746c97ddcba136297a54a9a0b6eee1533448
mekdim12 <jrmekdim50@gmail.com>
2024-04-17 10:44:59 +0300

    Refactor connectivity_controller.dart to handle internet connection status and display appropriate messages

--- lib/controller/connectivity_controller.dart
+++ lib/controller/connectivity_controller.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../components/pages/internet_connection_lost_page.dart';
 
 class NetworkController extends GetxController {
   final Connectivity _connectivity = Connectivity();
@@ -13,22 +14,28 @@ class NetworkController extends GetxController {
 
   void _update_connection_status(ConnectivityResult connectivityResult) {
     if (connectivityResult == ConnectivityResult.none) {
-      Get.rawSnackbar(
-        isDismissible: false,
-        backgroundColor: const Color.fromARGB(164, 124, 77, 255),
-        duration: const Duration(days: 1),
-        margin: EdgeInsets.zero,
-        snackStyle: SnackStyle.GROUNDED,
-        messageText: const Center(
-          child: Text("Connection to internet is lost",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 14)),
-        ),
-      );
+      Get.to(() => NoInternetPage(), preventDuplicates: false);
     } else {
       if (Get.isSnackbarOpen) {
         Get.closeCurrentSnackbar();
       }
+      if (Get.currentRoute == '/NoInternetPage') {
+        Get.back();
+      }
     }
   }
 }
+
+
+      // Get.rawSnackbar(
+      //   isDismissible: false,
+      //   backgroundColor: const Color.fromARGB(164, 124, 77, 255),
+      //   duration: const Duration(days: 1),
+      //   margin: EdgeInsets.zero,
+      //   snackStyle: SnackStyle.GROUNDED,
+      //   messageText: const Center(
+      //     child: Text("Connection to internet is lost",
+      //         textAlign: TextAlign.center,
+      //         style: TextStyle(color: Colors.white, fontSize: 14)),
+      //   ),
+      // );
